4cc39accf13154cc4d1eb4b491085aa713c4a328
eis <eis@iki.fi>
2016-08-17 15:38:57 +0300

    Added simple startup test

---
+++ src/test/java/hello/ApplicationTest.java
@@ -0,0 +1,10 @@
+package hello;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+    @Test
+    public void testApplicationStartup() {
+        Application.main(new String[]{});
+    }
+}
