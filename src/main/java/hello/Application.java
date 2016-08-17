91be462d2cdd240c5d301193e6e908beb75541e8
eis <eis@iki.fi>
2016-08-17 16:28:07 +0300

    disable startup logs in hopes to get travis build to work

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,7 +10,9 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setLogStartupInfo(false);
+        ApplicationContext ctx = app.run(args);
 
         System.out.println("Let's inspect the beans provided by Spring Boot:");
 
