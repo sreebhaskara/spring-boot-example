65bc78dab0ad67507e44c3fc7126af5fbe305802
eis <eis@iki.fi>
2016-12-08 15:37:24 +0200

    example exception routes

--- src/main/java/example/hello/HelloController.java
+++ src/main/java/example/hello/HelloController.java
@@ -1,8 +1,10 @@
 package example.hello;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 public class HelloController {
@@ -12,4 +14,15 @@ public class HelloController {
         return "Greetings from Spring Boot!";
     }
 
+    @RequestMapping(value = "/500/**")
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+    public String internalError() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping(value = "/400/**")
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public String badRequest() {
+        return "Greetings from Spring Boot!";
+    }
 }
