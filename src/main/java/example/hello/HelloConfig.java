5278f06d9bb0739dd3ab7275f0834b587510e51e
eis <eis@iki.fi>
2016-12-23 23:31:08 +0200

    added forgotten configuration annotation

--- src/main/java/example/hello/HelloConfig.java
+++ src/main/java/example/hello/HelloConfig.java
@@ -1,7 +1,9 @@
 package example.hello;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class HelloConfig {
     @Bean
     public HelloController helloController() {
