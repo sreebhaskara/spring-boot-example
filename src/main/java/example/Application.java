62a719c3c02d6d931b487721677133af658ee032
eis <eis@iki.fi>
2016-12-23 23:31:29 +0200

    removed unneeded clutter from main configuration

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,18 +1,12 @@
 package example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-import example.counter.CounterConfig;
-import example.hello.HelloConfig;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableAutoConfiguration 
 @EnableSwagger2
-@Import({CounterConfig.class, HelloConfig.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
